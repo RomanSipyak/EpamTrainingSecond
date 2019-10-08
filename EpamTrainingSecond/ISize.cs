c79b994d343ca02c18c4ae1df0d04797a7e37260
Roman <romansipyak@gmail.com>
2019-10-08 15:07:17 +0300

    add first homework

---
+++ EpamTrainingSecond/ISize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTrainingSecond
+{
+    interface ISize
+    {
+         int Height { get; set; }
+         int Width { get; set; }
+
+         void Perimetr();
+        
+    }
+}
---
+++ EpamTrainingSecond/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTrainingSecond
+{
+    struct Rectangle : ISize, ICoordinates
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        Rectangle(int Height, int Width, int X, int Y)
+        {
+            this.Height = Height;
+            this.Width = Width;
+            this.X = X;
+            this.Y = Y;
+        }
+
+        void ISize.Perimetr()
+        {
+            Console.WriteLine(2 * (Height + Width));
+        }
+        
+    }
+}
