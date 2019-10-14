7c7747880fdba98723ed6a1bed79e04b1ecda35e
Roman <romansipyak@gmail.com>
2019-10-14 23:29:16 +0300

    Add RunProject And RunImplementation for trainning1

---
+++ RunProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using PrinterHelpers;
+using PrinterHelpers.PrintersImplementations;
+using RunProject.RunImplementations.Training1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, IRun> menu = new Dictionary<string, IRun>();
+            menu.Add("T1TaskAll", new RunnerForAllTasks(new ConsolePrinter()));
+            bool exit = true;
+            do
+            {
+                Console.WriteLine("If you want run Training1 AkkTask write T1TaskAll ");
+                string key = Console.ReadLine();
+
+                if (menu.ContainsKey(key))
+                {
+                    exit = menu[key].Run();
+                }
+                else
+                {
+                    exit = false;
+                }
+            } while (exit);
+        }
+    }
+}
---
+++ RunProject/RunImplementations/Training1/RunnerForAllTasks.cs
@@ -0,0 +1,40 @@
+﻿using PrinterHelpers;
+using PrinterHelpers.PrintersImplementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trainig1.Task1;
+using Trainig1.Task3;
+using Trainig1.Task4;
+using Trainig1.Task5;
+
+namespace RunProject.RunImplementations.Training1
+{
+    public class RunnerForAllTasks : IRun
+    {
+        public IPrinter Printer { get; set; } = new ConsolePrinter();
+
+        public RunnerForAllTasks(IPrinter printer)
+        {
+            Printer = printer;
+        }
+        public bool Run()
+        {
+            Person person = new Person("Roma", "Sypiak", 21);
+            Printer.Writeline(person.GetElder(15));
+            Printer.Writeline(person.GetElder(21));
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            Printer.Writeline(MonthColection.GetMonthByNumber(n));
+            foreach (var value in Enum<Colors>.SortedValues)
+            {
+                Console.WriteLine(value);
+            }
+            Printer.Writeline($"Min = > {LongRangeTask.OutputLongRange(LongRange.Min)}");
+            Printer.Writeline($"Max = > {LongRangeTask.OutputLongRange(LongRange.Max)}");
+            Console.ReadKey();
+            return true;
+        }
+    }
+}
