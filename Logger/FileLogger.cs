57b27c2e90acee26e9f44ef1284623ce6960e07e
Roman <romansipyak@gmail.com>
2019-10-17 23:59:51 +0300

    Add simple logger

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logger
+{
+    public class FileLogger : ILogger
+    {
+        string PathOfFile { get; set; } = ConfigurationManager.AppSettings["PathToLog"].ToString();
+
+        public void ReadMessage()
+        {
+
+            using (StreamReader sr = new StreamReader(PathOfFile))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public void WriteMessage(Exception e)
+        {
+            if (!File.Exists(PathOfFile))
+            {
+                File.Create(PathOfFile);
+            }
+            using (StreamWriter sw = File.AppendText(PathOfFile))
+            {
+                sw.WriteLine(e.Message);
+            }
+        }
+    }
+}
