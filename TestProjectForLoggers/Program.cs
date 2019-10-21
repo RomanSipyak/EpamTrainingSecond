75469eb617e7242357f97381080e71ff01ec08e9
Roman <romansipyak@gmail.com>
2019-10-21 17:09:46 +0300

    Change Name ConsoleApp1 ==> TestProjectForLoggers

---
+++ TestProjectForLoggers/Program.cs
@@ -0,0 +1,72 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Training5.Task1;
+
+namespace TestProjectForLoggers
+{
+    class Program
+    {
+        public static object MyLogger { get; private set; }
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            //ShowerDirectory sd = new ShowerDirectory();
+            //String filesAndDerectories = sd.ShowerDirectoryAndFiles("C:\\Users\\GOOD\\source\\repos\\EpamTrainingSecond",new StringBuilder());
+            //Console.WriteLine(filesAndDerectories);
+            //Console.ReadLine();
+            //MyLogger logger = new MyLogger();
+            //logger.SrcLogger = new DataBaseLogger();
+            //logger.SrcLogger.WriteMessage(new Exception());
+            //logger.SrcLogger.ReadMessage();
+            //try
+            //{
+            //    Logger.Info("Hello world");
+
+            //    throw new IndexOutOfRangeException();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Logger.Error(ex, "Goodbye cruel world");
+            //    Console.WriteLine("sdfdsfdsfsd");
+            //}
+            //foreach (var drive in DriveInfo.GetDrives())
+            //{
+            //    Console.WriteLine(drive.Name);
+            //}
+
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car { Marck = "asds", Speed = 12 });
+            cars.Add(new Car { Marck = "13", Speed = 12 });
+            CarSerializator serializer = new CarSerializator();
+            serializer.SerializeForBinary(cars);
+            List<Car> carsDeserialize = new List<Car>();
+            carsDeserialize = serializer.DeserializeForBinary();
+            foreach (var car in carsDeserialize)
+            {
+                Console.WriteLine(car);
+            }
+
+            serializer.SerializeForXML(cars);
+            carsDeserialize = new List<Car>();
+            carsDeserialize = serializer.DeserializeForXML();
+            foreach (var car in carsDeserialize)
+            {
+                Console.WriteLine(car);
+            }
+            string jsonCars = serializer.SerializeForJson(cars);
+            carsDeserialize = new List<Car>();
+            carsDeserialize = serializer.DeserializeForJson(jsonCars);
+            foreach (var car in carsDeserialize)
+            {
+                Console.WriteLine(car);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestProjectForLoggers/ShowerDirectory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestProjectForLoggers
+{
+    class ShowerDirectory
+    {
+        public string ShowerDirectoryAndFiles(string path, StringBuilder stringbuilder)
+        {
+            try
+            {
+               
+                DirectoryInfo directory = new DirectoryInfo(path);
+                DirectoryInfo[] subDirectories = directory.GetDirectories();
+                FileInfo[] files = directory.GetFiles();
+                string searchPart = "width";
+                Regex regex = new Regex(@"\w*"+ searchPart + @"\w*", RegexOptions.IgnoreCase);
+                //Console.Write(FileWriter(files));
+                stringbuilder.Append(FileWriter(files));
+                //Console.Write(SearchInString(FileWriter(files), searchPart));
+                if (directory.GetDirectories().Length == 0)
+                {
+                   
+                }
+                else
+                {
+                    foreach (DirectoryInfo dir in subDirectories)
+                    {
+                        //Console.WriteLine("{0} ==> {1}", dir.FullName, dir);
+                        stringbuilder.AppendLine($"{dir.FullName} ==> {dir}");
+                        ShowerDirectoryAndFiles(dir.FullName,stringbuilder);
+                    }
+                }
+                return stringbuilder.ToString();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public string FileWriter(FileInfo[] filesinfo)
+        {
+            StringBuilder stringbuilder = new StringBuilder();
+            foreach (FileInfo file in filesinfo)
+            {
+                stringbuilder.AppendLine($"{file.DirectoryName} ==> {file}");
+            }
+            return stringbuilder.ToString();
+        }
+        public string SearchInString(String strings, String nameOfFile)
+        {
+            StringBuilder stringbuilder = new StringBuilder();
+            string[] liness = Regex.Split(strings, "\r\n");
+            Regex regex = new Regex(@"\w*" + nameOfFile + @"\w*", RegexOptions.IgnoreCase);
+            foreach (String line in liness)
+            {
+                if (regex.IsMatch(line))
+                {
+                    stringbuilder.AppendLine($"We find your file in ==>{line.Replace("\r","").Replace("\n","")}");
+                }
+            }
+            return stringbuilder.ToString();
+        }
+    }
+}
+//C:\Users\GOOD\source\repos\EpamTrainingSecond
