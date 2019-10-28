96d66340b94bbbf2d944088ad8b271892deadc02
Roman <romansipyak@gmail.com>
2019-10-28 22:52:24 +0300

    Add shower without I/O

---
+++ Training7/FolderTaskVariant2/FileNameComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training7.FolderTaskVariant2
+{
+    public class FileNameComparer : IEqualityComparer<FileInfo>
+    {
+        public bool Equals(FileInfo x, FileInfo y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return string.Equals(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public int GetHashCode(FileInfo obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
---
+++ Training7/FolderTaskVariant2/ShowerDirectory.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="ShowerDirectory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Training7.FolderTaskVariant2
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+
+    public class ShowerDirectory
+    {
+        public HashSet<FileInfo> DirectoryInfo1 { get; private set; }
+
+        public HashSet<FileInfo> DirectoryInfo2 { get; private set; }
+
+        public ShowerDirectory()
+        {
+            string[] infoAboutFirstFoder = ShowerDirectory.ShowAllFiles(ConfigurationManager.AppSettings["PathFolderFirst"].ToString());
+            string[] infoAboutSecondFolder = ShowerDirectory.ShowAllFiles(ConfigurationManager.AppSettings["PathFolderSecond"].ToString());
+            this.DirectoryInfo1 = new HashSet<FileInfo>();
+            this.DirectoryInfo2 = new HashSet<FileInfo>();
+
+            foreach (string res in infoAboutFirstFoder)
+            {
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(res);
+                this.DirectoryInfo1.Add(fileInfo);
+            }
+
+            foreach (string res in infoAboutSecondFolder)
+            {
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(res);
+                this.DirectoryInfo2.Add(fileInfo);
+            }
+        }
+
+        public static string[] ShowAllFiles(string path)
+        {
+            return Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+        }
+
+        public HashSet<FileInfo> SymmetricalDifference()
+        {
+            HashSet<FileInfo> except1 = this.DirectoryInfo1.Except(this.DirectoryInfo2, new FileNameComparer()).ToHashSet();
+            HashSet<FileInfo> except2 = this.DirectoryInfo2.Except(this.DirectoryInfo1, new FileNameComparer()).ToHashSet();
+            HashSet<FileInfo> resultWithUniqueValues = except1.Concat(except2).ToHashSet();
+            return resultWithUniqueValues;
+        }
+
+        public HashSet<FileInfo> ExpectDirectory()
+        {
+            HashSet<FileInfo> resultWithUniqueValues = this.DirectoryInfo1.Except(this.DirectoryInfo2, new FileNameComparer()).ToHashSet();
+            return resultWithUniqueValues;
+        }
+
+        public HashSet<FileInfo> Intersection()
+        {
+            HashSet<FileInfo> resultWithUniqueValues = this.DirectoryInfo1.Intersect(this.DirectoryInfo2, new FileNameComparer()).ToHashSet();
+            return resultWithUniqueValues;
+        }
+    }
+}
