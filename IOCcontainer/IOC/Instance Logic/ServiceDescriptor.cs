278dc180a95a4578066e46957788794576d7a124
Roman <romansipyak@gmail.com>
2019-12-03 20:35:18 +0300

    Add IOC container

---
+++ IOCcontainer/IOC/Instance Logic/ServiceDescriptor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCcontainer.IOC.Instance_Logic
+{
+    public enum ServiceLifeTime
+    {
+        Singleton,
+        Transient
+    }
+
+    public class ServiceDescriptor
+    {
+        public Type ServiceType { get; }
+
+        public Type ImplementationType { get; }
+
+        public object Implementation { get; internal set; }
+
+        public ServiceLifeTime Lifetime { get; }
+
+        public ServiceDescriptor(object implementation, ServiceLifeTime lifetime)
+        {
+            ServiceType = implementation.GetType();
+            Implementation = implementation;
+            Lifetime = lifetime;
+        }
+
+        public ServiceDescriptor(Type serviceType, ServiceLifeTime lifetime)
+        {
+            ServiceType = serviceType;
+            Lifetime = lifetime;
+        }
+
+        public ServiceDescriptor(Type serviceType, Type implementationType, ServiceLifeTime lifetime)
+        {
+            ServiceType = serviceType;
+            Lifetime = lifetime;
+            ImplementationType = implementationType;
+        }
+    }
+}
---
+++ IOCcontainer/IOC/Return Instance Logic/DiContainer.cs
@@ -0,0 +1,66 @@
+﻿using IOCcontainer.IOC.Instance_Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCcontainer.IOC.Return_Instance_Logic
+{
+    public class DiContainer
+    {
+        public readonly List<ServiceDescriptor> _serviceDescriptors;
+
+        public DiContainer( ref List<ServiceDescriptor> serviceDescriptors)
+        {
+            _serviceDescriptors = serviceDescriptors;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            var desciptor = _serviceDescriptors.SingleOrDefault(x => x.ServiceType == serviceType);
+
+            if (desciptor == null)
+            {
+                throw new Exception($"Service of type {serviceType.Name} isn't registered");
+            }
+
+            if (desciptor.Implementation != null)
+            {
+                return desciptor.Implementation;
+            }
+
+            var actualType = desciptor.ImplementationType ?? desciptor.ServiceType;
+
+            if (actualType.IsAbstract || actualType.IsInterface)
+            {
+                throw new Exception("Cannot instantiate abstract classes or interfaces");
+            }
+
+            object implementation = InstanceByreflection(actualType);
+
+            if (desciptor.Lifetime == ServiceLifeTime.Singleton)
+            {
+                desciptor.Implementation = implementation;
+            }
+
+            return implementation;
+        }
+
+        private object InstanceByreflection(Type actualType)
+        {
+            var constructorInfo = actualType.GetConstructors().First();
+
+            var parameters = constructorInfo.GetParameters()
+                .Select(x => GetService(x.ParameterType)).ToArray();
+
+            var implementation = Activator.CreateInstance(actualType, parameters);
+            return implementation;
+        }
+
+        public T GetService<T>()
+        {
+            return (T)GetService(typeof(T));
+        }
+    }
+}
